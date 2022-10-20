6aeb9befe7f6d342ff65c87bc7357fce3b8a559d
Izz Hafeez <37770501+mynameizzhafeez@users.noreply.github.com>
2022-10-20 22:41:05 +0800

    Delete target directory

--- target/debug/deps/regex_warrior-13cfc09b66c599e5.d
+++
@@ -1,6 +0,0 @@
-/Users/izzhafeez/Documents/Rust/regex-warrior/target/debug/deps/regex_warrior-13cfc09b66c599e5: src/main.rs src/words.rs
-
-/Users/izzhafeez/Documents/Rust/regex-warrior/target/debug/deps/regex_warrior-13cfc09b66c599e5.d: src/main.rs src/words.rs
-
-src/main.rs:
-src/words.rs:
--- target/debug/deps/regex_warrior-3136f0089c97a3d1.d
+++
@@ -1,6 +0,0 @@
-/Users/izzhafeez/Documents/Rust/regex-warrior/target/debug/deps/regex_warrior-3136f0089c97a3d1: src/main.rs src/words.rs
-
-/Users/izzhafeez/Documents/Rust/regex-warrior/target/debug/deps/regex_warrior-3136f0089c97a3d1.d: src/main.rs src/words.rs
-
-src/main.rs:
-src/words.rs:
